ecc5b65715e059457e7c6412e58f7bdcf96c978d
Christophe Craig <christophe.craig@viacesi.fr>
2023-03-25 22:43:42 +0100

    migrations init

---
+++ chauss4-back/database/migrations/2023_03_25_174854_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_number');
+            $table->decimal('total_amount', 10, 2);
+            $table->enum('status', ['en_attente', 'expediee', 'livree', 'annulee'])->default('en_attente');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
---
+++ chauss4-back/database/migrations/2023_03_25_174905_create_baskets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBasketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('baskets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->decimal('total_amount', 10, 2);
+            $table->enum('status', ['en_cours', 'abandonne'])->default('en_cours');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('baskets');
+    }
+}
